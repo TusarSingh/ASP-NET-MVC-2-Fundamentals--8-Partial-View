6e8644f6e69d7f05c17aaa75a3cb018bae3d7ea0
Tusar Singh <rts0941@gmail.com>
2023-10-10 16:05:41 +0530

    Add Partial View And Use It

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Tarun" };
+
+         
+
+            return View(movie);
+        }
+
+    }
+}
